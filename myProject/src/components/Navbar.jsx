09b1f8ab8b5271dda0b87458170df909e178ca79
Macu de Sarasqueta <macu.desarasqueta@gmail.com>
2025-01-03 22:08:50 +0100

    tailwind

--- myProject/src/components/Navbar.jsx
+++ myProject/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
 import CartWidget from './CartWidget';
 
-
 export default function Navbar () {
     return (
         <nav>
-            <img src="/images/Untitled design.png" width={30} alt="logo" />
-            <li style = {{display: 'inline-block'}}>
-                <a href="">&nbsp;Inicio </a>
-                <a href="">Mates </a>
-                <a href="">Bombillas </a>
-                <a href="">Yerberas</a>
-            </li>
-            <CartWidget/>
-        </nav>
+            <div>
+                <div>
+                    <img src="/images/Untitled design.png" width={30} alt="logo" />
+                    <a href="#" className="hover:text-gray-400"></a>
+                </div>
+
+                {/* Links */}
+                <div className="hidden md:flex space-x-6">
+                <a href="#" className="hover:text-gray-400">
+                    Mates
+                </a>
+                <a href="#" className="hover:text-gray-400">
+                    Bombillas
+                </a>
+                <a href="#" className="hover:text-gray-400">
+                    Yerberas
+                </a>
+                <a href="#" className="hover:text-gray-400">
+                    Combos
+                </a>
+                </div>
+                <CartWidget/>
+            </div>
+        </nav>   
     );
 };
